544193b5771dfc007e3e41214ca7e7971b79b0cc
skybreakbb <willistaylora@gmail.com>
2023-07-26 19:43:30 -0500

    started working on server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'ad0f5162-bb60-4241-b917-9ee7c40cc770',
+    cookie: {
+        maxAge: 60 * 60 * 1000 * 24,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extented: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () =>
+        console.log(`Server running on port ${PORT}. http://localhost:${PORT}`
+        )
+    );
+});
